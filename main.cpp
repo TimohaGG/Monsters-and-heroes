e4465d34a1138e90a66476478df8e8515e201e27
Tim <timoha22.a11@gmail.com>
2022-06-08 14:05:00 +0200

    Finisging Point

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
 ﻿#include"Lib/Lib.h"
-//#include"Classes/MyClass/MyClass.h"
 #include"Classes/Point/Point.h"
 int main() {
 	RUS;
 	TIME;
-
-	//------------MyClass------------
-	/*int size = 2;
-	int size2 = 6;
-	MyClass a(size);
-	MyClass b(size2);
-	
-	MyClass c= a / b;
-	cout << "A: ";
-	a.PrintArr();
-	cout << "B: ";
-	b.PrintArr();
-	cout << "C: ";
-	c.PrintArr();*/
 
 	//------------Point------------
 	Point a(2,4);
