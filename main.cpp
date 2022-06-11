d419a13bdea2ddd4a4cfe28e38514ca43b3f0148
Tim <timoha22.a11@gmail.com>
2022-06-11 21:03:11 +0200

    Deleting comment

--- main.cpp
+++ main.cpp
@@ -14,28 +14,3 @@ int main() {
 	 pro.PrintProfessor();
 }
 
-//
-//bool noRepeat(int*& arr, int number, int gamesAvailable) {
-//	for (size_t i = 0; i < gamesAvailable; i++)
-//	{
-//		if (arr[i] == number)
-//			return false;
-//	}
-//	return true;
-//}
-//
-//
-//void User::randNumber(int*& arrNumbers, int gamesAvailable, int userGamesAmount) {
-//	//int buf = gamesAvailable;
-//	arrNumbers[0] = rand() % gamesAvailable;
-//	for (size_t i = 1; i < userGamesAmount; i++)
-//	{
-//		int number;
-//		do {
-//			number = rand() % gamesAvailable;
-//			//cout << number << endl;
-//		} while (!noRepeat(arrNumbers, number, gamesAvailable));
-//		arrNumbers[i] = number;
-//
-//	}
-//}
