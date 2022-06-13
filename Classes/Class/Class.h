bb20ee1e7f19c258507e0db929805d9addf4efe7
Tim <timoha22.a11@gmail.com>
2022-06-13 11:13:26 +0200

    dgf

---
+++ Classes/Class/Class.h
@@ -0,0 +1,30 @@
+#pragma once
+#include"../../Lib/Lib.h"
+class Msg {
+public:
+	Msg(string msg) {
+		this->msg = msg;
+	}
+	virtual string GetMsg() {
+		return msg;
+	}
+private:
+	string msg;
+};
+
+class BrMsg : public Msg{
+public:
+	BrMsg(string msg) : Msg(msg) {
+	}
+	string GetMsg() override
+	{
+		return "[" + ::Msg::GetMsg() + "]";
+	}
+};
+
+class Printer{
+public:
+	void Print(Msg* msg) {
+		cout << msg->GetMsg() << endl;
+	}
+};
