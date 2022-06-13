2e4ba473613ca0d4fded6ca51203da86d86b2037
Tim <timoha22.a11@gmail.com>
2022-06-13 10:29:33 +0200

    asd

--- Classes/Class/Class.h
+++ Classes/Class/Class.h
@@ -1,11 +1,43 @@
 #pragma once
-class Class
-{
+#include"../../Lib/Lib.h"
+class Weaphon {					//Абстрактный класс 
 public:
-	Class();
-	~Class();
-
-private:
-
+	virtual void Shoot() = 0;
 };
 
+class Gun : public Weaphon
+{
+public:
+	 void Shoot() override;
+};
+
+class Machinegun : public Gun {
+public:
+	void Shoot() override;
+};
+
+class Bazooka : public Weaphon {
+public:
+	void Shoot() override;
+};
+
+class Bomb : public Weaphon {
+public:
+	void Shoot() override;
+};
+
+class Player  {
+public:
+	void Shoot(Weaphon* weaphon);
+	void Print();
+	Player();
+	Player(string name);
+	Player(string name, int age);
+	
+
+private:
+	static int amount;
+	string name;
+	int age;
+	int id;
+};
